0c194717072991305fbc592a2bbac6284107ed14
Cai Dixon <city.art.girl@gmail.com>
2023-05-10 15:21:04 +0900

    im not good at front end but it works

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
+Restaurant.destroy_all
+
 5.times do
   restaurant = Restaurant.new
   restaurant.name = Faker::Restaurant.name
   restaurant.address = Faker::Address.street_address
+  restaurant.phone_number = Faker::PhoneNumber.cell_phone
   restaurant.category = Restaurant::CATEGORIES.sample
   restaurant.save
 end
